3f214bf0139ec224e69c6a18b856998cee72204e
Kieran Goodacre <kierangoodacre@kierans-mbp.home>
2017-03-19 19:38:40 +0000

    bike now knows when it is full

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,13 @@
 class DockingStation
+  DEFAULT_CAPACITY = 10
 
-  def initialize
+  def initialize (options = {})
+    @capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
     @bikes = []
+  end
+
+  def full?
+    bike_count == @capacity
   end
 
   def bike_count
